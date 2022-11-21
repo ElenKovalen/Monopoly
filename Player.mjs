32516e9c0bc744308a29ac7710c1775bcc71efce
izhigal <ivan.zhigalov@nextspace.nz>
2022-11-21 20:21:22 +1100

    get method added

--- Player.mjs
+++ Player.mjs
@@ -2,4 +2,8 @@ export class Player {
     constructor(id){
        this.id = id;
     }
+
+    get bestCard() {
+        return this.cards.reduce((prev, current) => prev.value < current.value ? current : prev);
+    }
 }
