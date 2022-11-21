8aec207109dbf1dc9f91a1b1048f4ec5244eac2a
Elena Kovalenko <aszhigalova@gmail.com>
2022-11-21 16:57:05 +1100

    propertySets description added

--- Card.mjs
+++ Card.mjs
@@ -28,4 +28,17 @@ export class PropertyCard extends Card {
     }
 }
 
+export const propertySets = {
+    brawn: [1, 2],
+    mint: [1, 2],
+    blue: [3, 8],
+    skyBlue: [1, 2, 3],
+    purple:[1, 2, 4],
+    orange: [1, 3, 5],
+    red: [2, 3, 6],
+    yellow: [2, 4, 6],
+    green: [2, 4, 7],
+    black: [1, 2, 3, 4]
+};
+
 const brown = new PropertyCard(1, "brown", "Old Kent Road" [1, 2]);
